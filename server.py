9f50c67a76d01a03fd048f3beeedfbef7cb6ee16
Kelly Petrino <kellypetrino@MacBook-Pro-5.local>
2020-03-24 03:29:35 -0400

    cas

--- server.py
+++ server.py
@@ -2,22 +2,28 @@ import os
 from sys import argv
 
 from flask import Flask, render_template, session, redirect, send_from_directory
-# from flask_cas import CAS
-# from flask_cas import login
-# from flask_cas import logout
-# from flask_cas import login_required
-# import logging
+from flask_cas import CAS, login_required
 
 app = Flask(__name__)
-# cas = CAS(app, '/cas')
-# app.config['CAS_SERVER'] = 'https://fed.princeton.edu/cas/'
-# app.config['CAS_AFTER_LOGIN'] = 'secure'
+app.secret_key = os.urandom(24)
+cas = CAS(app, '/cas')
+app.config['CAS_SERVER'] = 'https://fed.princeton.edu/cas/'
+app.config['CAS_AFTER_LOGIN'] = 'home'
 
 # DATABASE_URL = 'postgres://bgeduosfkxunua:79c8ea392b4ee24827466fffa6186fcd606e8fd61aed0f72474965c5058c00da@ec2-54-80-184-43.compute-1.amazonaws.com:5432/dbratmlm42smtt'
 
+# C = CASClient.CASClient()
+# netid = C.Authenticate()
+
 @app.route("/")
+@login_required
+def route_root():
+    return "Testing %s" % cas.attributes['cas:displayName']
+
+@app.route("/splash")
 def main():
-    return render_template("splash.html")
+    return "Hello %s" % netid
+    # return render_template("splash.html")
 
 @app.route("/home")
 def home():
