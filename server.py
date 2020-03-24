1b84c59b13d1e8d0f12738578771371e4419c890
Kelly Petrino <kellypetrino@MacBook-Pro-5.local>
2020-03-24 09:24:47 -0400

    queue

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import os
 from sys import argv
 import urllib
-# import psycopg2
+import psycopg2
 
 import flask
-from flask import Flask, render_template, session, redirect, send_from_directory
+from flask import Flask, render_template, session, redirect, send_from_directory, request
 from flask_cas import CAS, login_required, login, logout
 
 from flask_wtf import FlaskForm
@@ -16,14 +16,27 @@ cas = CAS(app)
 app.config['CAS_SERVER'] = 'https://fed.princeton.edu/'
 app.config['CAS_AFTER_LOGIN'] = 'home'
 
-# DATABASE_URL = 'postgres://bgeduosfkxunua:79c8ea392b4ee24827466fffa6186fcd606e8fd61aed0f72474965c5058c00da@ec2-54-80-184-43.compute-1.amazonaws.com:5432/dbratmlm42smtt'
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# cursor = conn.cursor()
-# conn.commit()
+DATABASE_URL = 'postgres://bgeduosfkxunua:79c8ea392b4ee24827466fffa6186fcd606e8fd61aed0f72474965c5058c00da@ec2-54-80-184-43.compute-1.amazonaws.com:5432/dbratmlm42smtt'
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
+conn.commit()
+
+cursor.execute("DROP TABLE IF EXISTS queue")
+conn.commit()
+cursor.execute(
+    """
+    CREATE TABLE queue (
+        name VARCHAR(50) NOT NULL PRIMARY KEY,
+        prob VARCHAR(50) NOT NULL,
+        time INTEGER NOT NULL,
+        descrip VARCHAR(100)
+    )"""
+)
+conn.commit()
+
 
 @app.route("/") 
 def main():
-    # return "SPLASH PAGE GOES HERE"
     return render_template("splash.html")
 
 class SignUpForm(FlaskForm):
@@ -33,11 +46,17 @@ class SignUpForm(FlaskForm):
     descrip = StringField('Description')
     submit = SubmitField('Submit')
 
-@app.route("/home")
+@app.route("/home", methods=['GET','POST'])
 @login_required
 def home():
     form = SignUpForm()
-    return render_template("index.html", netid = cas.username, form = form)
+    if form.is_submitted():
+        result = request.form
+        cursor.execute("INSERT INTO queue VALUES ('%s', '%s', %s. '%s'", (result("name"), result("prob"), result("time"), result("descrip")))
+        conn.commit()
+    cursor.execute("SELECT name, prob, descrip FROM queue")
+    queue = cursor.fetchall()
+    return render_template("index.html", netid=cas.username, form=form, queue=queue)
 
 
 if __name__ == "__main__":
