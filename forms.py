2ed2789ead1930a738c05e7b95a6f6949162d0a0
Kelly Petrino <kellypetrino@MacBook-Pro-5.local>
2020-04-20 04:50:49 -0400

    please work

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField, HiddenField, SelectMultipleField
+
+class SignUpForm(FlaskForm):
+    name = StringField('Name')
+    prob = SelectField('Problem Type', choices=[('conc','Conceptual'), ('debug', 'Debugging')])
+    time = SelectField('Estimated Time', choices=[('5', 'Quick'), ('9', 'Medium'), ('12', 'Long')])
+    descrip = SelectMultipleField('Description', choices=[('1', 'Testing'), ('2', 'API'), ('3', 'Data Structure'), ('4', 'Algorithm'), ('5', 'Exception'), ('6', 'Getting Started')])
+    submit = SubmitField('Submit')
+
+class RemoveForm(FlaskForm):
+    name = HiddenField('Name')
+    submit = SubmitField('X')
--- server.py
+++ server.py
@@ -8,22 +8,23 @@ from flask import Flask, render_template, session, redirect, send_from_directory
 from flask_cas import CAS, login_required, login, logout
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
+from forms import SignUpForm, RemoveForm
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-cas = CAS(app)
-app.config['CAS_SERVER'] = 'https://fed.princeton.edu/'
-app.config['CAS_AFTER_LOGIN'] = 'home'
+# cas = CAS(app)
+# app.config['CAS_SERVER'] = 'https://fed.princeton.edu/'
+# app.config['CAS_AFTER_LOGIN'] = 'home'
 
  
-# conn = psycopg2.connect(host="localhost",database="ohlocal", user="postgres", password="sqlpass")
-DATABASE_URL = 'postgres://bgeduosfkxunua:79c8ea392b4ee24827466fffa6186fcd606e8fd61aed0f72474965c5058c00da@ec2-54-80-184-43.compute-1.amazonaws.com:5432/dbratmlm42smtt'
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(host="localhost",database="ohlocal", user="postgres", password="sqlpass")
+# DATABASE_URL = 'postgres://bgeduosfkxunua:79c8ea392b4ee24827466fffa6186fcd606e8fd61aed0f72474965c5058c00da@ec2-54-80-184-43.compute-1.amazonaws.com:5432/dbratmlm42smtt'
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
 conn.commit()
 
+# Create table to store the current queue of students
 cursor.execute("DROP TABLE IF EXISTS queue")
 conn.commit()
 cursor.execute(
@@ -37,33 +38,84 @@ cursor.execute(
 )
 conn.commit()
 
+# Create a key-value store of problem descriptions
+problems = {1: 'Testing', 2: 'API', 3: 'Data Structures', 4: 'Algorithm', 5: 'Exception', 6: 'Getting Started'}
+
 
 @app.route("/") 
 def main():
     return render_template("splash.html")
 
-class SignUpForm(FlaskForm):
-    name = StringField('Name')
-    prob = SelectField('Problem Type', choices=[('conc','Conceptual'), ('debug', 'Debugging')])
-    time = SelectField('Estimated Time', choices=[('5', 'Quick'), ('9', 'Medium'), ('12', 'Long')])
-    descrip = StringField('Description')
-    submit = SubmitField('Submit')
 
 @app.route("/home", methods=['GET','POST'])
-@login_required
+# @login_required
 def home():
     form = SignUpForm()
+    form2 = RemoveForm()
+    if form.is_submitted():
+        queue = get_queue()
+        result = request.form.to_dict()
+        print(type(result))
+        print(result)
+        # print(form.descrip.data)
+        # print(type(form.descrip.data))
+        cursor.execute("INSERT INTO queue VALUES (%s, %s, %s, %s)", (result["name"], result["prob"], result["time"], form.descrip.data))
+        conn.commit()
+        if len(queue) > 0: 
+            sim = 0
+            match = queue[0]
+            for stu in queue:
+                sim_temp = jaccard(result, stu)
+                if sim_temp > sim:
+                    sim = sim_temp
+                    match = stu
+            # print(match)
+            # print(type(match))
+            # print(type(match[3]))
+            return render_template("index.html", form=form, form2=form2, queue=get_queue(), wait=get_wait(), match=match) 
+    elif form2.is_submitted():
+        result = request.form
+        cursor.execute("DELETE FROM queue WHERE name = '%s'" % result["name"])
+        conn.commit()
+    return render_template("index.html", form=form, form2=form2, queue=get_queue(), wait=get_wait())
+
+
+@app.route("/ta_portal", methods=['GET','POST'])
+#@login_required
+def ta_portal():
+    form = RemoveForm()
     if form.is_submitted():
         result = request.form
-        cursor.execute("INSERT INTO queue VALUES (%s, %s, %s, %s)", (result["name"], result["prob"], result["time"], result["descrip"]))
+        cursor.execute("DELETE FROM queue WHERE name = '%s'" % result["name"])
         conn.commit()
+    return render_template("ta_portal.html", form=form, queue=get_queue(), wait=get_wait())
+
+def get_queue():
     cursor.execute("SELECT name, prob, time, descrip FROM queue")
     queue = cursor.fetchall()
+    return queue
+
+def get_wait():
+    queue = get_queue()
     wait = 0
     for stu in queue:
         wait = wait + int(stu[2])
-    return render_template("index.html", netid=cas.username, form=form, queue=queue, wait=wait)
+    return wait
 
+def jaccard(a, b):
+    cp = 0
+    pa = 0
+    ap = 0
+    for i in problems:
+        if str(i) in a["descrip"]:
+            if str(i) in b[3]:
+                cp = cp + 1
+            else:
+                pa = pa + 1
+        elif str(i) in b[3]:
+            ap = ap + 1
+    sim = cp / (cp + pa + ap)
+    return sim
 
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5000))
